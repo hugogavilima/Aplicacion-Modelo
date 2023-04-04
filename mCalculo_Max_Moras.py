888c9209ead4441122eac512b242b166bb833ae2
AquilesBailo <hugoisraelgavilima@gmail.com>
2023-04-04 16:03:04 -0500

    Nueva aplicacion

---
+++ mCalculo_Max_Moras.py
@@ -0,0 +1,134 @@
+import json
+import pandas as pd
+import numpy as np
+
+
+def Calculo_Max_Mora(gender, edad, provincia, jefe, EC, hijos, ans_bin_hijos, sede, grado, 
+                     carrera, materias, monto_fin, monto_couta, laboral, patrono, 
+                     propiedades, vehiculos, hipoteca, ingreso, por_beca, por_matricula, 
+                     monto_beca, monto_matricula):
+    
+    with open('pesos.json', encoding='utf-8-sig') as f:
+        data = json.load(f)    
+    intercept = float(data[0]['Intercept'])
+    
+    #GENERO
+    w_gender = float(data[0]["Genero" + gender])      
+    
+    #EDAD 
+    edad_int = [pd.Interval(left=20, right=21, closed='left'), 
+                pd.Interval(left=22, right=25, closed='left'), 
+                pd.Interval(left=25, right=31, closed='left'), 
+                pd.Interval(left=31, right=71, closed='left'),
+                pd.Interval(left=0, right=20, closed='left')]
+    
+    wh_edad = np.arange(len(edad_int))[[edad in x for x in edad_int]][0]
+    w_edad = float(data[0]["q_Edad_"+ str(wh_edad)])
+    
+    #PROVINCIA
+    w_provincia = float(data[0]["Provincia" + provincia])
+    
+    #JEFE
+    w_jefe = float(data[0]["jefehogar" + str(int(jefe))])
+    
+    #ESTADO CIVIL
+    w_EC = float(data[0]["EstadoCivil" + EC])
+    
+    #HIJOS
+    w_hijos = 0
+    if ans_bin_hijos != "No registra":
+        w_hijos = float(data[0]["hijos"])*hijos
+        
+    #SEDE
+    w_sede = float(data[0]["SEDE" + sede])
+    
+    #GRADO
+    w_grado = float(data[0]["Grado" + grado])
+    
+    #Carrera
+    w_carrera = float(data[0]["Carrera" + carrera])
+    
+    #Materias
+    w_materias = float(data[0]["`Materias matriculadas`"])*materias
+    
+    #monto_fin
+    monto_fin_int = [pd.Interval(left=1.87e+05, right=2.43e+05, closed='left'), 
+                    pd.Interval(left=2.43e+05, right=3.1e+05, closed='left'), 
+                    pd.Interval(left=3.1e+05, right=4.31e+05, closed='left'), 
+                    pd.Interval(left=4.31e+05, right=3.62e+06, closed='left'),
+                    pd.Interval(left=0, right=1.87e+05, closed='left')]
+    
+    wh_monto_fin = np.arange(len(monto_fin_int))[[monto_fin in x for x in monto_fin_int]][0]
+    w_monto_fin = float(data[0]["q_monto_financiado_"+ str(wh_monto_fin)])
+
+    #monto_couta
+    monto_couta_int = [pd.Interval(left=0, right=5.74e+04, closed='left'), 
+                    pd.Interval(left=5.74e+04, right=7.64e+04, closed='left'), 
+                    pd.Interval(left=7.64e+04, right=9.76e+04, closed='left'), 
+                    pd.Interval(left=9.76e+04, right=1.38e+05, closed='left'),
+                    pd.Interval(left=1.38e+05, right=1.14e+06, closed='left')]
+    
+    wh_monto_couta = np.arange(len(monto_couta_int))[[monto_couta in x for x in monto_couta_int]][0]
+    w_monto_couta = float(data[0]["q_monto_cuota_"+ str(wh_monto_couta)])
+    
+    #Estatus Laboral + Tipo Patrono
+    w_laboral = 0
+    w_patrono = float(data[0]["Tipo_PatronoNo Trabaja"])
+    if laboral == "SI":
+        w_laboral = float(data[0]["status_lab1"])
+        w_patrono = float(data[0]["Tipo_Patrono"+ patrono])
+        
+    #Propiedades
+    w_propiedades = float(data[0]["propiedades"])*propiedades
+    
+    #Vehiculos
+    w_vehiculos = float(data[0]["vehiculos"])*vehiculos
+        
+    #Vehiculos
+    w_hipoteca = float(data[0]["Hipoteca1"])*hipoteca
+    
+    #INGRESO
+    ingreso_int = [pd.Interval(left=0, right=4e+05, closed='left'), 
+                    pd.Interval(left=4e+05, right=5.12e+05, closed='left'), 
+                    pd.Interval(left=5.12e+05, right=6.7e+05, closed='left'), 
+                    pd.Interval(left=6.7e+05, right=1.25e+06, closed='left'),
+                    pd.Interval(left=1.25e+06, right=4.96e+06, closed='left')]
+    
+    wh_ingreso = np.arange(len(ingreso_int))[[ingreso in x for x in ingreso_int]][0]
+    w_ingreso = float(data[0]["q_ingreso_"+ str(wh_ingreso)])
+    
+    
+    #PORCENTAJE BECA
+    por_beca_int = [pd.Interval(left=-10, right=0, closed='left'), 
+                    pd.Interval(left=0, right=20, closed='left'), 
+                    pd.Interval(left=20, right=35, closed='left'), 
+                    pd.Interval(left=35, right=100, closed='left')]
+    
+    wh_por_beca = np.arange(len(por_beca_int))[[por_beca in x for x in por_beca_int]][0]
+    w_por_beca = float(data[0]["q_beca_"+ str(wh_por_beca)])
+    
+    #PORCENTAJE MATRICULA
+    por_matricula_int = [pd.Interval(left=-10, right=0, closed='left'), 
+                    pd.Interval(left=0, right=15, closed='left'), 
+                    pd.Interval(left=15, right=20, closed='left'), 
+                    pd.Interval(left=20, right=35, closed='left'),
+                    pd.Interval(left=35, right=100, closed='left')]
+    
+    wh_por_matricula = np.arange(len(por_matricula_int))[[por_matricula in x for x in por_matricula_int]][0]
+    w_por_matricula = float(data[0]["q_descuentoMat_"+ str(wh_por_matricula)])
+    
+    #Descuento Matricula
+    w_monto_beca = float(data[0]["DescMat"])*monto_beca
+        
+    #Descuento Colegiatura
+    w_monto_matricula = float(data[0]["DescCol"])*monto_matricula
+    
+    
+    max_dias = sum([w_monto_matricula, w_monto_beca, w_por_matricula, w_por_beca,
+                    w_ingreso, w_hipoteca, w_vehiculos, w_propiedades, w_patrono,
+                    w_laboral, w_monto_couta, w_monto_fin, w_materias, w_carrera,
+                    w_grado, w_sede, w_hijos, w_EC, w_jefe, w_provincia, w_edad,
+                    w_gender, intercept])    
+    
+    
+    return int(max_dias)
---
+++ run.py
@@ -0,0 +1,160 @@
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+from PIL import Image
+from mCalculo_Max_Moras import*
+
+ 
+    
+st.set_page_config(layout="wide", page_title="Máximo Días de Mora")
+
+
+st.write("## Máximo Días de Mora")
+st.sidebar.write("## Calculadora")
+st.sidebar.write(
+    "Esta aplicación te permite calcular el máximo días de mora de una persona dada su información academica, civil y financiera tanto del estudiante y su representante."
+    )
+
+opciones = ['Universidad Latina de Costa Rica', 'Universidad Americana']
+ans_box = st.sidebar.selectbox('Porfavor elija la universidad correspondiente:', opciones)
+
+if ans_box == opciones[0]:
+    image = Image.open("Universidad Latina de Costa Rica.png")
+    
+if ans_box == opciones[1]:
+    image = Image.open("Universidad Americana.png")
+    
+    
+##Steamos correctamente la imagen
+
+width, height = image.size
+max_width = 300
+if width > max_width:
+    height *= max_width / width
+    width = max_width
+image = image.resize((int(width), int(height)))
+x_offset = 20
+y_offset = 20
+
+# Añadir la imagen a la aplicación
+st.sidebar.image(image, 
+                use_column_width=False, 
+                clamp=True, 
+                width=width, 
+                caption=ans_box, 
+                output_format='PNG', 
+                channels='RGB')
+
+col1, col2, col3 = st.columns(3)
+
+# Agregamos tres cajas de selección a la primera columna
+
+
+##Opciones para cada variable
+gender = ['Femenino', 'Masculino', 'No registra']
+sede = ['Heredia', 'Cañas', 'San Pedro', 'Ciudad Neily', 'Grecia',
+       'Guápiles', 'Pérez Zeledón', 'Santa Cruz']
+provincia = ['HEREDIA', 'ALAJUELA', 'PUNTARENAS', 'SAN JOSE', 'No registra',
+       'LIMON', 'CARTAGO', 'GUANACASTE']
+patrono = ['Gobierno', 'Privado', 'Independiente']
+EC = ['Divorciado(a)', 'Casado(a)', 'Soltero(a)', 'Viudez']
+hijos = [ "NO", "SI", "No registra"]
+bin_lab = [ "NO","SI", "No registra"]
+ans_bin_hijos = "No registra"
+ans_bin_lab = "No registra"
+carrera = ['RELACIONES PÚBLICAS', 'INGENIERÍA INDUSTRIAL', 'TERAPIA FÍSICA',
+       'ENFERMERÍA', 'ARQUITECTURA', 'ARQUITECTURA Y URBANISMO',
+       'INGENIERÍA ELECTROMECÁNICA', 'SEGURIDAD INFORMÁTICA',
+       'INGENIERÍA CIVIL', 'INGENIERÍA EN SISTEMAS COMPUTACIONALES',
+       'LA ENSEÑANZA DEL INGLÉS', 'ADMINISTRACIÓN DE NEGOCIOS', 'DERECHO',
+       'OPTOMETRÍA', 'MEDICINA Y CIRUGÍA', 'ODONTOLOGÍA',
+       'DISEÑO GRÁFICO', 'COMUNICACIÓN DIGITAL', 'TRABAJO SOCIAL',
+       'PERIODISMO', 'PUBLICIDAD', 'ADMINISTRACIÓN DE EMPRESAS HOTELERAS',
+       'PSICOLOGÍA', 'INGENIERÍA ELECTRÓNICA',
+       'DISEÑO Y DECORACIÓN DE INTERIORES', 'CONTADURÍA',
+       'INGENIERÍA MECÁNICA Y ADMINISTRACIÓN', 'ANIMACIÓN DIGITAL',
+       'GASTRONOMÍA', 'CIENCIAS DE LA EDUCACIÓN PREESCOLAR', 'FARMACIA',
+       'COMUNICACIÓN DE MERCADEO',
+       'TECNOLOGÍAS DE INFORMACIÓN PARA LA GESTIÓN DE LOS NEGOCIOS',
+       'CIENCIAS BIOLÓGICAS', 'CIENCIAS DE LA EDUCACIÓN',
+       'CONTADURÍA PÚBLICA', 'INGENIERÍA EN ELECTROMEDICINA',
+       'INGENIERÍA EN TELEMÁTICA', 'ECONOMÍA',
+       'INGENIERÍA DE SISTEMAS INFORMÁTICOS', 'TURISMO SOSTENIBLE',
+       'CIENCIAS DE LA COMUNICACIÓN COLECTIVA', 'INGENIERÍA DEL SOFTWARE',
+       'NUTRICIÓN', 'RELACIONES INTERNACIONALES',
+       'INGENIERÍA DE SISTEMAS',
+       'INGENIERÍA ELECTRÓNICA Y COMUNICACIONES',
+       'ADMINISTRACIÓN DE LA HOSPITALIDAD']
+grado = ['BACHILLERATO', 'LICENCIATURA']
+
+with col1:
+    st.write("### Civil")
+    ans_gender = st.selectbox('Género', gender)
+    ans_edad = st.slider('Edad', 18, 100)
+    ans_provincia = st.selectbox('Provincia', provincia)
+    st.write("")
+    ans_jefe = st.checkbox('Es jefe de hogar')
+    st.write("")
+    ans_EC = st.selectbox('Estado Civil', EC)
+    ans_bin_hijos = st.selectbox('Tiene hijos', hijos)
+    ans_hijos = 0
+    
+    if ans_bin_hijos == "SI":
+        ans_hijos = st.slider('Numero de Hijos', 0, 5)
+        
+
+    
+     
+    
+
+
+# Agregamos tres cajas de selección a la segunda columna
+with col2:
+    st.write("### Académica")
+    ans_sede = st.selectbox('SEDE', sede)
+    ans_grado =st.selectbox('Grado mayor alcanzado', grado)
+    ans_carrera = st.selectbox('Carrera en Curso', carrera)
+    ans_materias = st.slider('Materias Matriculadas', 0, 7)
+    ans_monto_fin = st.number_input('Escriba el monto financiado: ', min_value=0.0, max_value=1e7, step=0.1)
+    ans_monto_couta = st.number_input('Escriba el valor de la couta: ', min_value=0.0, max_value=1e7, step=0.1)
+       
+    
+    
+with col3:
+    st.write("### Financiera")
+    ans_bin_lab = st.selectbox('Actualmente trabaja', bin_lab)
+    ans_patrono = "No trabaja"
+    
+    if ans_bin_lab == "SI":
+        ans_patrono = st.selectbox('Empleador', patrono)
+    
+    ans_propiedades = st.slider('Numero de Propiedades', 0, 20)
+    ans_vehiculos = st.slider('Numero de Vehiculos', 0, 40)
+    st.write("")
+    ans_hipoteca = st.checkbox('Tiene un Credito Hipotecario')
+    st.write("")
+    ingreso = st.number_input('Escriba el ingreso estimado: ', min_value=0.0, max_value=1e15, step=0.1)
+
+st.write("### Descuentos")
+ans_beca = st.slider('Porcentaje de Descuento Colegiatura ', 0, 100)
+ans_matricula_porc = st.slider('Porcentaje de Descuento Matricula ', 0, 100)
+ans_beca_monto = st.number_input('Escriba el valor total del descuento en la colegiatura: ', min_value=0.0, max_value=1e7, step=0.1)
+ans_matricula_monto = st.number_input('Escriba el valor total del descuento en la matricula: ', min_value=0.0, max_value=1e7, step=0.1)
+
+if st.button('Mostrar cuadro emergente'):
+    max_days = Calculo_Max_Mora(ans_gender, ans_edad, ans_provincia, ans_jefe, ans_EC, ans_hijos, ans_bin_hijos, ans_sede, ans_grado, 
+                                ans_carrera, ans_materias, ans_monto_fin, ans_monto_couta, ans_bin_lab, ans_patrono, 
+                                ans_propiedades, ans_vehiculos, ans_hipoteca, ingreso, ans_beca, ans_matricula_porc, 
+                                ans_beca_monto, ans_matricula_monto)
+    st.markdown(
+        f"""
+        <div style='background-color:#ff3333; padding:20px; border-radius: 10px; '>
+            <span style='color:white; font-size: 30px'>{"El usuario tendrá un máximo de "}</span><br>
+            <span style='color:white; font-size: 200px'>{max_days}</span>
+            <span style='color:white; font-size: 89px'>{" días mora"}</span>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+
